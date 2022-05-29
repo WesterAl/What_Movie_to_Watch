78d221e9f4bd7e7b4ca515a1e7a8834eade62818
WesterAl <albin.westerlund@gmail.com>
2022-05-29 19:17:56 +0100

    Added all pages and links to them from home. Link to movie details is on the page search results

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { HomeComponent } from './home/home.component';
 
 import { RouterModule, Routes } from '@angular/router';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { CreateListComponent } from './create-list/create-list.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 //Routes within the web application
 //const routes: Routes = [
@@ -20,13 +25,26 @@ import { TopBarComponent } from './top-bar/top-bar.component';
     AppComponent,
     ProfileComponent,
     HomeComponent,
-    TopBarComponent
+    TopBarComponent,
+    CreateListComponent,
+    LoginComponent,
+    RegistrationComponent,
+    SearchResultsComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: 'profile', component: ProfileComponent },
+      { path: 'createlist', component: CreateListComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'registration', component: RegistrationComponent },
+      { path: 'searchresults', component: SearchResultsComponent },
+      { path: 'moviedetails', component: MovieDetailsComponent },
+
+
+
     ])
     
   ],
